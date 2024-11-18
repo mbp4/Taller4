ecb9cf81573d7275b87d18ac2f4d5cb11be628df
mbp4 <mblanpon@myuax.com>
2024-11-18 10:57:47 +0100

    sensor y cambio de color

--- app/src/main/java/com/example/taller4/Sensor.kt
+++ app/src/main/java/com/example/taller4/Sensor.kt
@@ -70,13 +70,19 @@ class Sensor: AppCompatActivity(), SensorEventListener {
 
             val movimiento = Math.sqrt((x * x + y * y + z * z).toDouble())
 
-            if (movimiento > 1) {
+            val GRAVITY = 9.8
+            val UMBRAL = 2.0
+
+            if (Math.abs(movimiento - GRAVITY) > UMBRAL) {
+
                 imagen.setBackgroundColor(resources.getColor(R.color.verdePastel))
             } else {
+
                 imagen.setBackgroundColor(resources.getColor(R.color.white))
             }
         }
     }
+
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
        // No se necesita implementar en este caso
